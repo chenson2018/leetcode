bba1dc4372ea4ee58dce81ef87d2520ad336a811
Chris Henson <chrishenson.net@gmail.com>
2024-09-05 19:57:15 -0400

    704

---
+++ src/bin/0704.rs
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/binary-search/
+
+use std::cmp::Ordering::*;
+struct Solution;
+
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        Self::search_slice(&nums, target, 0).unwrap_or(-1)
+    }
+
+    pub fn search_slice(nums : &[i32], target : i32, idx_left : usize) -> Option<i32> {
+        let mid = nums.len() / 2;
+        match target.cmp(&nums[mid]) {
+            Less | Greater if mid == 0 => None,
+            Less => Self::search_slice(&nums[..mid], target, idx_left),
+            Greater => Self::search_slice(&nums[mid..], target, idx_left + mid),
+            Equal => Some((mid + idx_left) as i32),
+        }
+    }
+}
+
+fn main() {
+    // println!("{}", Solution::search(vec![-1,0,3,5,9,12], 9));
+    println!("{}", Solution::search(vec![-1,0,3,5,9,12], 2));
+}
