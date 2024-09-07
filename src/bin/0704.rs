d8c3d19ce4cf01ed31152bc71095c17208e2d5e9
Chris Henson <chrishenson.net@gmail.com>
2024-09-06 20:14:08 -0400

    move cast

--- src/bin/0704.rs
+++ src/bin/0704.rs
@@ -5,16 +5,18 @@ struct Solution;
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        Self::search_slice(&nums, target, 0).unwrap_or(-1)
+        Self::search_slice(&nums, target, 0)
+            .map(|x| x as i32)
+            .unwrap_or(-1)
     }
 
-    pub fn search_slice(nums: &[i32], target: i32, idx_left: usize) -> Option<i32> {
+    pub fn search_slice(nums: &[i32], target: i32, idx_left: usize) -> Option<usize> {
         let mid = nums.len() / 2;
         match target.cmp(&nums[mid]) {
             Less | Greater if mid == 0 => None,
             Less => Self::search_slice(&nums[..mid], target, idx_left),
             Greater => Self::search_slice(&nums[mid..], target, idx_left + mid),
-            Equal => Some((mid + idx_left) as i32),
+            Equal => Some(mid + idx_left),
         }
     }
 }
