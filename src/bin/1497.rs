8b64aa4829cd0540679a5c551ae5da1a4c68ef30
Chris Henson <chrishenson.net@gmail.com>
2024-10-01 19:22:36 -0400

    hashmap version

--- src/bin/1497.rs
+++ src/bin/1497.rs
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/
 
+use std::collections::HashMap;
+
 struct Solution;
 
 impl Solution {
@@ -14,6 +16,19 @@ impl Solution {
             }
         }
         counts[0] % 2 == 0
+    }
+
+    pub fn can_arrange_alt(arr: Vec<i32>, k: i32) -> bool {
+        let mut counts = HashMap::new();
+        for x in arr {
+            *counts.entry(x.rem_euclid(k)).or_default() += 1;
+        }
+        for i in 1..k / 2 + 1 {
+            if counts.get(&i) != counts.get(&(k - i)) {
+                return false;
+            }
+        }
+        counts.get(&0).map_or(true, |x| x % 2 == 0)
     }
 }
 
